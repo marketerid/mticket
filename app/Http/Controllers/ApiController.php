7931e39e4cf9005cd36308284e514aaa0062cd80
Ridwan <ridwan@digitalstrategy.id>
2019-10-24 12:02:58 +0700

    u

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -48,7 +48,7 @@ class ApiController extends Controller
     {
         $midtrans   = new MidtransService();
         $response   = $midtrans->notification();
-        // Log::error("Midtrans Notif: " . json_encode($response));
+        Log::error("Midtrans Notif: " . response()->json($request->all()));
 
         if (is_null($response['order_id'])) {
             // something error
