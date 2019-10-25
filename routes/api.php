04b193762dfd2504e8fcbc8b62187d954e88bcb2
Ridwan <ridwan@digitalstrategy.id>
2019-10-25 08:19:33 +0700

    url

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 Route::post('checkout-midtrans', 'ApiController@checkoutMidtrans');
 Route::post('save-payment-midtrans/{id?}', 'ApiController@savePaymentMidtrans');
-Route::post('notify-midtrans', 'ApiController@paymentNotificationMidtrans');
+Route::any('notify-midtrans', 'ApiController@paymentNotificationMidtrans');
 
 Route::get('get-seminar', 'ApiController@getSeminar');
 Route::get('change-status-event', 'ApiController@changeStatusEvent');
